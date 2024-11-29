e9759157ea8ac24b19860407b8757f0b8ac17940
kvan7 <kvan.valve@gmail.com>
2024-11-29 14:42:51 -0600

    moar chang

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,svelte,html}'],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			colors: {
